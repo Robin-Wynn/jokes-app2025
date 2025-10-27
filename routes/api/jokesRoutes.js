c2dfaf16a55e7f72337a66788d57548178b3039c
Robin-Wynn <wynnrobin95@gmail.com>
2025-10-27 11:43:38 -0500

    used axios to get jokes

--- routes/api/jokesRoutes.js
+++ routes/api/jokesRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 const PORT = process.env.PORT || 3001
 
 // http://localhost:3001/jokes
 router.get('/', (req, res)=> {
-    res.send('This works')
+    // res.send('This works')
+    const url = `https://api.sampleapis.com/jokes/goodJokes`
+
+    axios.get(url) 
+        .then(resp => console.log(resp))
+        .then(res.send('📦data received📦'))
 })
 
 module.exports = router
